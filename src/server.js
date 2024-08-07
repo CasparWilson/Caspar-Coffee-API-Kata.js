178cd2a9fc5048f483001da7a145c05a89078bd0
Caspar <casparwilson111@gmail.com>
2024-08-07 15:04:12 +0100

    end point for get customer by ID, and stamp customer by ID added

--- src/server.js
+++ src/server.js
@@ -4,12 +4,6 @@ import morgan from "morgan";
 
 const app = express();
 
-const customers = ["Bukola", "Caspar", "Dana", "Olu", "Paul"];
-
-app.get("/customers", function (_req, res) {
-    res.json(customers);
-});
-
 //allow morgan logger to get access to each request before and after our handlers
 app.use(morgan("dev"));
 //auto-include CORS headers to allow consumption of our content by in-browser js loaded from elsewhere
@@ -20,7 +14,7 @@ app.use(express.json());
 //use the environment variable PORT, or 4000 as a fallback
 const PORT = process.env.PORT ?? 4000;
 
-const customersloyalty = [
+const customersLoyalty = [
     { name: "Bukola", customerID: 1, stamps: 2 },
     { name: "Caspar", customerID: 2, stamps: 2 },
     { name: "Dana", customerID: 3, stamps: 2 },
@@ -31,11 +25,31 @@ const customersloyalty = [
 let nextCustomerID = 6;
 
 app.get("/customerLoyalty", function (_req, res) {
-    res.json(customersloyalty);
+    res.json(customersLoyalty);
+});
+
+app.get("/customerLoyalty/:customerID", (req, res) => {
+    const customerID = parseInt(req.params.customerID);
+    const customerByID = customersLoyalty.find((customer) => {
+        return customer.customerID === customerID;
+    });
+    console.log(customerByID);
+    if (!customerID) {
+        console.error("No customer with that ID found for /GET");
+        res.status(400).json({
+            error: "No customer found with that ID",
+        });
+        return;
+    }
+    res.json(customerByID);
 });
 
 app.post("/customerLoyalty", (req, res) => {
-    const newCustomer = { name: req.body, customerID: nextCustomerID, stamps: 0 };
+    const newCustomer = {
+        name: req.body,
+        customerID: nextCustomerID,
+        stamps: 0,
+    };
     if (!newCustomer) {
         console.error("No new customer name received in POST/");
         res.status(400).json({
@@ -45,9 +59,30 @@ app.post("/customerLoyalty", (req, res) => {
     }
     nextCustomerID++;
     console.log({ newCustomer });
-    customersloyalty.push(newCustomer);
+    customersLoyalty.push(newCustomer);
 
     res.json({ outcome: "success", message: newCustomer });
+});
+
+app.patch("/customerLoyalty/:customerID/stamp", (req, res) => {
+    const customerToStampID = parseInt(req.params.customerID);
+    const customerToStamp = customersLoyalty.find((customer) => {
+        return customer.customerID === customerToStampID;
+        // console.log(customerToStamp);
+    });
+    if (!customerToStamp) {
+        console.error("No customer with that ID found for /PATCH");
+        res.status(400).json({
+            error: "No customer found with that ID",
+        });
+        return;
+    }
+    console.log(customerToStamp);
+    customerToStamp.stamps++;
+    res.json({
+        outcome: "succesfully stamped",
+        message: `${customerToStamp.name} has ${customerToStamp.stamps} stamps.`,
+    });
 });
 
 app.listen(PORT, () => {
